7861d5c622288d264a6dc786703ce810a883aef9
jermiedomingo <8jermiedomingo@gmail.com>
2019-01-01 14:41:34 +0400

    adding more sprites

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-## HTML 5 games practice
+## Me trying to build browser games that someday will break the internet (or at least your browser)
--- sprites/game.js
+++ sprites/game.js
@@ -2,34 +2,80 @@ class Game {
   constructor() {
     this.canvas = document.getElementById("game");
     this.context = this.canvas.getContext("2d");
+    this.sprites = [];
 
     this.spriteImage = new Image();
     this.spriteImage.src = "flower.png";
 
-    const game = this;
-    this.spriteImage.onload = function() {
-      const options = {
-        context: game.context,
-        width: this.width,
-        height: this.height,
-        image: this
-      };
-
-      game.sprite = new Sprite(options);
-      game.sprite.render();
+    this.spriteImage.onload = () => {
+      this.lastRefreshTime = Date.now();
+      this.spawn(); //first spawn
+      this.refresh();
     };
+  }
+
+  //function to create a new sprite
+  spawn() {
+    const sprite = new Sprite({
+      context: this.context,
+      x: Math.random() * this.canvas.width,
+      y: Math.random() * this.canvas.height,
+      width: this.spriteImage.width,
+      height: this.spriteImage.height,
+      image: this.spriteImage
+    });
+
+    // add the new sprite to the list of sprites
+    //for re-render
+    this.sprites.push(sprite);
+    this.sinceLastSpawn = 0;
+  }
+
+  refresh() {
+    const now = Date.now();
+    const dt = (now - this.lastRefreshTime) / 1000; //elapsed time in S
+
+    this.update(dt); //update to spawn shit again
+    this.lastRefreshTime = now;
+
+    //browser function that accepts a callback
+    // that gets called when the browser refreshes the screen
+    // so this gets called infinitely during browser refresh (most of the time 60fps)
+    requestAnimationFrame(() => {
+      this.refresh();
+    });
+  }
+
+  update(dt) {
+    this.sinceLastSpawn += dt;
+    // if 1 sec elapsed, spawn a new sprite
+    if (this.sinceLastSpawn > 1) {
+      this.spawn(); //spawn some shit
+      this.render(); //re-render the canvas
+    }
+  }
+
+  //render all sprites
+  // apparently the canvas gets cleared and re-rendered?
+  render() {
+    console.log("render");
+    for (let sprite of this.sprites) {
+      sprite.render();
+    }
   }
 }
 
 class Sprite {
-  constructor({ context, width, height, image }) {
+  constructor({ context, width, height, image, x, y }) {
     this.context = context;
     this.width = width;
     this.height = height;
     this.image = image;
+    this.x = x;
+    this.y = y;
   }
 
   render() {
-    this.context.drawImage(this.image, 200, 100);
+    this.context.drawImage(this.image, this.x, this.y);
   }
 }
