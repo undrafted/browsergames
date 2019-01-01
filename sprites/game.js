28b28596dd6dd29da2315650fe4040e26679f985
jermiedomingo <8jermiedomingo@gmail.com>
2019-01-01 13:34:08 +0400

    sprites intro

---
+++ sprites/game.js
@@ -0,0 +1,35 @@
+class Game {
+  constructor() {
+    this.canvas = document.getElementById("game");
+    this.context = this.canvas.getContext("2d");
+
+    this.spriteImage = new Image();
+    this.spriteImage.src = "flower.png";
+
+    const game = this;
+    this.spriteImage.onload = function() {
+      const options = {
+        context: game.context,
+        width: this.width,
+        height: this.height,
+        image: this
+      };
+
+      game.sprite = new Sprite(options);
+      game.sprite.render();
+    };
+  }
+}
+
+class Sprite {
+  constructor({ context, width, height, image }) {
+    this.context = context;
+    this.width = width;
+    this.height = height;
+    this.image = image;
+  }
+
+  render() {
+    this.context.drawImage(this.image, 200, 100);
+  }
+}
